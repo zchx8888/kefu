e6329d01d980dfcdc46968191ecb45ab5caaae9f
zchx8888 <zchx88633@outlook.com>
2026-07-11 18:48:21 +0000

    修复管理员后台菜单加载不全

--- src/main/java/com/kefu/controller/AuthController.java
+++ src/main/java/com/kefu/controller/AuthController.java
@@ -32,8 +32,12 @@ public class AuthController {
             data.put("token", token);
             data.put("account", account);
             data.put("roles", new String[]{"ADMIN"});
+            data.put("role", "admin");
             data.put("balance", "999999");
             data.put("expireDate", "永久有效");
+            data.put("expireDays", 365000); // large positive for permanent
+            data.put("isExpired", false);
+            data.put("canUseChat", true);
             return ResponseEntity.ok(Map.of("code", 0, "msg", "", "data", data));
         }
 
@@ -72,10 +76,15 @@ public class AuthController {
         user.put("account", username);
         if ("hxzc33".equals(username)) {
             user.put("roles", new String[]{"ADMIN"});
+            user.put("role", "admin");
             user.put("balance", "999999");
             user.put("expireDate", "永久有效");
+            user.put("expireDays", 365000);
+            user.put("isExpired", false);
+            user.put("canUseChat", true);
         } else {
             user.put("roles", new String[]{"USER"});
+            user.put("role", "user");
             user.put("balance", "0");
             user.put("expireDate", "未知");
         }
